13291c2731989126535a56e988ad11626f4aa21b
damnnou <skdamn@outlook.com>
2024-02-26 13:08:50 +0300

    feat: add img to mock db

--- src/pages/products-page/ProductsPage.tsx
+++ src/pages/products-page/ProductsPage.tsx
@@ -6,12 +6,15 @@ const ProductsPage = () => {
         {
             id: 1,
             name: 'THEM SKATES 908 POWDER',
+            img: 'https://image.goat.com/750/attachments/product_template_pictures/images/097/208/456/original/1228708_00.png.png',
         },
         {
             id: 2,
             name: 'Кроссовки крутые',
+            img: 'https://image.goat.com/750/attachments/product_template_pictures/images/087/983/615/original/1131716_00.png.png',
         },
     ];
+
     return (
         <>
             {products.map((product) => (
