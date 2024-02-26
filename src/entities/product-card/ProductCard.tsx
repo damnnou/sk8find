56ed8c3de2c3e078a103b8a63623fe50f6a4fc51
Fem1on <pantyad@bk.ru>
2024-02-26 13:46:43 +0300

    style: cards

--- src/entities/product-card/ProductCard.tsx
+++ src/entities/product-card/ProductCard.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import cls from './ProductCard.module.scss';
+import React from "react";
+import cls from "./ProductCard.module.scss";
 
-const ProductCard = ({ id, name }) => {
-    return (
-        <div className={cls.productCard}>
-            <p className={cls.productCard__logo}>Logo</p>
-            <div className={cls.productCard__info}>
-                <h3 className={cls.productCard__name}>{name}</h3>
-                <p className={cls.productCard__model}>Model</p>
-                <p className={cls.productCard__price}>$200</p>
-                <a className={cls.productCard__btn}>GET IT NOW {'--->'} </a>
-            </div>
+const ProductCard = ({ id, name, img }) => {
+  return (
+    <div className={cls.productCard}>
+      <div className={cls.productCard__right}>
+        <img src={img} className={cls.productCard__right__img} />
+      </div>
+      <div className={cls.productCard__left}>
+        <p className={cls.productCard__logo}>Logo</p>
+        <div className={cls.productCard__info}>
+          <h3 className={cls.productCard__name}>{name}</h3>
+          <p className={cls.productCard__model}>Model</p>
+          <p className={cls.productCard__price}>$200</p>
+          <a className={cls.productCard__btn}>GET IT NOW {"--->"} </a>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
--- src/pages/products-page/ProductsPage.tsx
+++ src/pages/products-page/ProductsPage.tsx
@@ -1,27 +1,37 @@
-import React from 'react';
-import ProductCard from '../../entities/product-card/ProductCard';
+import React from "react";
+import ProductCard from "../../entities/product-card/ProductCard";
 
 const ProductsPage = () => {
-    const products = [
-        {
-            id: 1,
-            name: 'THEM SKATES 908 POWDER',
-            img: 'https://image.goat.com/750/attachments/product_template_pictures/images/097/208/456/original/1228708_00.png.png',
-        },
-        {
-            id: 2,
-            name: 'Кроссовки крутые',
-            img: 'https://image.goat.com/750/attachments/product_template_pictures/images/087/983/615/original/1131716_00.png.png',
-        },
-    ];
+  const products = [
+    {
+      id: 1,
+      name: "THEM SKATES 908 POWDER",
+      img: "https://image.goat.com/750/attachments/product_template_pictures/images/097/208/456/original/1228708_00.png.png",
+    },
+    {
+      id: 2,
+      name: "Кроссовки крутые",
+      img: "https://image.goat.com/750/attachments/product_template_pictures/images/087/983/615/original/1131716_00.png.png",
+    },
+    {
+      id: 3,
+      name: "Кроссовкt крутые",
+      img: "https://image.goat.com/750/attachments/product_template_pictures/images/087/983/615/original/1131716_00.png.png",
+    },
+  ];
 
-    return (
-        <>
-            {products.map((product) => (
-                <ProductCard key={product.id} {...product} />
-            ))}
-        </>
-    );
+  return (
+    <>
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          id={product.id}
+          name={product.name}
+          img={product.img}
+        />
+      ))}
+    </>
+  );
 };
 
 export default ProductsPage;
