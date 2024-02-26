9cc134200816975f6c08ba49de163c3152b2278f
damnnou <skdamn@outlook.com>
2024-02-26 13:44:50 +0300

    styles: add fonts

--- src/pages/products-page/ProductsPage.tsx
+++ src/pages/products-page/ProductsPage.tsx
@@ -6,11 +6,15 @@ const ProductsPage = () => {
         {
             id: 1,
             name: 'THEM SKATES 908 POWDER',
+            price: 200,
+            describe: 'MODEL BY ALEX BROSKOW',
             img: 'https://image.goat.com/750/attachments/product_template_pictures/images/097/208/456/original/1228708_00.png.png',
         },
         {
             id: 2,
-            name: 'Кроссовки крутые',
+            name: 'AIR JORDAN PRO NIGGER BLACK',
+            price: 150,
+            describe: 'MODEL BY CHURKA',
             img: 'https://image.goat.com/750/attachments/product_template_pictures/images/087/983/615/original/1131716_00.png.png',
         },
     ];
