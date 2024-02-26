cb65629e4d472fc491846cdfe6b906835d26d2de
damnnou <skdamn@outlook.com>
2024-02-26 15:05:13 +0300

    feat: add header

--- src/pages/products-page/ProductsPage.tsx
+++ src/pages/products-page/ProductsPage.tsx
@@ -1,36 +1,38 @@
-import React from "react";
-import ProductCard from "../../entities/product-card/ProductCard";
+import React from 'react';
+import ProductCard from '../../entities/product-card/ProductCard';
+import Header from '../../shared/layout/Header/Header';
 
 const ProductsPage = () => {
-  const products = [
-    {
-      id: 1,
-      name: "THEM SKATES 908 POWDER",
-      price: 200,
-      describe: "MODEL BY ALEX BROSKOW",
-      img: "https://image.goat.com/750/attachments/product_template_pictures/images/097/208/456/original/1228708_00.png.png",
-    },
-    {
-      id: 2,
-      name: "AIR JORDAN PRO NIGGER BLACK",
-      price: 150,
-      describe: "MODEL BY CHURKA",
-      img: "https://image.goat.com/750/attachments/product_template_pictures/images/087/983/615/original/1131716_00.png.png",
-    },
-  ];
+    const products = [
+        {
+            id: 1,
+            name: 'THEM SKATES 908 POWDER',
+            price: 200,
+            describe: 'MODEL BY ALEX BROSKOW',
+            img: 'https://image.goat.com/750/attachments/product_template_pictures/images/097/208/456/original/1228708_00.png.png',
+        },
+        {
+            id: 2,
+            name: 'AIR JORDAN PRO NIGGER BLACK',
+            price: 150,
+            describe: 'MODEL BY CHURKA',
+            img: 'https://image.goat.com/750/attachments/product_template_pictures/images/087/983/615/original/1131716_00.png.png',
+        },
+    ];
 
-  return (
-    <>
-      {products.map((product) => (
-        <ProductCard
-          key={product.id}
-          id={product.id}
-          name={product.name}
-          img={product.img}
-        />
-      ))}
-    </>
-  );
+    return (
+        <>
+            <Header />
+            {products.map((product) => (
+                <ProductCard
+                    key={product.id}
+                    id={product.id}
+                    name={product.name}
+                    img={product.img}
+                />
+            ))}
+        </>
+    );
 };
 
 export default ProductsPage;
