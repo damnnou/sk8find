4d6678bc769901606fa2c16df6deae3599ba677b
Fem1on <pantyad@bk.ru>
2024-02-29 19:21:39 +0300

    bug fix:create: productPageMenu

--- src/pages/products-page/ProductsPage.tsx
+++ src/pages/products-page/ProductsPage.tsx
@@ -1,68 +1,19 @@
-// import React, { useEffect, useState } from "react";
-// import ProductCard from "../../entities/product-card/ProductCard";
-// import Header from "../../shared/layout/Header/Header";
-// import cls from "./ProductsPage.module.scss";
-// import { getAllSneakers } from "../../api/get/sneakers";
-
-// const ProductsPage = () => {
-//   const [sneakers, setSneakers] = useState([]);
-
-//   useEffect(() => {
-//     getAllSneakers().then((data) => setSneakers(data));
-//   }, []);
-
-//   return (
-//     <>
-//       <Header />
-//       <div className={cls.productsPage}>
-//         {sneakers.map((product) => {
-//           console.log(product.title.length);
-//           let title = product.title.split(" ");
-//           title.length = 3;
-//           title = title.join(" ");
-
-//           console.log(title);
-//           return (
-//             <ProductCard
-//               key={product.title}
-//               id={product.id}
-//               name={title}
-//               img={product.img}
-//               describe={product.brand}
-//               price={product.price}
-//             />
-//           );
-//         })}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default ProductsPage;
-// ProductsPage.tsx
 import React, { useEffect, useState } from "react";
 import ProductCard from "../../entities/product-card/ProductCard";
 import Header from "../../shared/layout/Header/Header";
-import Menu from "../../menu/Menu";
 import cls from "./ProductsPage.module.scss";
 import { getAllSneakers } from "../../api/get/sneakers";
 
 const ProductsPage = () => {
   const [sneakers, setSneakers] = useState([]);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     getAllSneakers().then((data) => setSneakers(data));
   }, []);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
     <>
       <Header />
-      <Menu isOpen={isMenuOpen} onClose={toggleMenu} />
       <div className={cls.productsPage}>
         {sneakers.map((product) => {
           console.log(product.title.length);
